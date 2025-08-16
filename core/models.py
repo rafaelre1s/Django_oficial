210725ef264ae0b7fb2137aaf34167aaf216d985
rafaelre1s <rafael.arnaldo@outlook.com>
2025-08-16 11:49:02 +0200

    Django-admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
